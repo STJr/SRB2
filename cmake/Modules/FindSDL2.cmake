05f5ec664a9d10c88502d454f69a6ed87b90b028
Ronald Kinard <furyhunter600@gmail.com>
2015-01-27 23:46:39 -0600

    Initial CMake support pass
    Compiles the SDL2 target on Windows successfully

---
+++ cmake/Modules/FindSDL2.cmake
@@ -0,0 +1,34 @@
+# Find SDL2
+# Once done, this will define
+# 
+#  SDL2_FOUND - system has SDL2
+#  SDL2_INCLUDE_DIRS - SDL2 include directories
+#  SDL2_LIBRARIES - link libraries
+
+include(LibFindMacros)
+
+libfind_pkg_check_modules(SDL2_PKGCONF SDL2)
+
+# includes
+find_path(SDL2_INCLUDE_DIR
+	NAMES SDL.h
+	PATHS
+		${SDL2_PKGCONF_INCLUDE_DIRS}
+		"/usr/include/SDL2"
+		"/usr/local/include/SDL2"
+)
+
+# library
+find_library(SDL2_LIBRARY
+	NAMES SDL2
+	PATHS
+		${SDL2_PKGCONF_LIBRARY_DIRS}
+		"/usr/lib"
+		"/usr/local/lib"
+)
+
+
+# set include dir variables
+set(SDL2_PROCESS_INCLUDES SDL2_INCLUDE_DIR)
+set(SDL2_PROCESS_LIBS SDL2_LIBRARY)
+libfind_process(SDL2)
