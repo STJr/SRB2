3ff9bf757f944c00a81b055f5f6b03781bdac65f
wolfy852 <wolfy852@hotmail.com>
2016-01-03 21:58:45 -0600

    Update MODID for SRB2TD

--- src/doomdef.h
+++ src/doomdef.h
@@ -197,13 +197,13 @@ extern FILE *logstream;
 // The Modification ID; must be obtained from Inuyasha ( http://mb.srb2.org/private.php?do=newpm&u=2604 ).
 // DO NOT try to set this otherwise, or your modification will be unplayable through the Master Server.
 // "12" is the default mod ID for version 2.1
-#define MODID 12
+#define MODID 14
 
 // The Modification Version, starting from 1. Do not follow your version string for this,
 // it's only for detection of the version the player is using so the MS can alert them of an update.
 // Only set it higher, not lower, obviously.
 // Note that we use this to help keep internal testing in check; this is why v2.1.0 is not version "1".
-#define MODVERSION 19
+#define MODVERSION 1
 
 
 
