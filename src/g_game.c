8f0abb267edd493b58e875a08083af9c36ea3870
wolfy852 <wolfy852@hotmail.com>
2016-02-25 22:47:52 -0600

    Don't mix declarations and code

--- src/g_game.c
+++ src/g_game.c
@@ -2330,8 +2330,7 @@ void G_SpawnPlayer(INT32 playernum, boolean starpost)
 	P_MovePlayerToSpawn(playernum, spawnpoint);
 	
 #ifdef HAVE_BLUA 
-	player_t *p = &players[playernum];
-	LUAh_PlayerSpawn(p); // Lua hook for player spawning :)
+	LUAh_PlayerSpawn(&players[playernum]); // Lua hook for player spawning :)
 #endif
 
 }
