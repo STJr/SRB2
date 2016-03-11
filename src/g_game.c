a4026199001c59cf45280af8d2c922c103ff9021
Yukita Mayako <catgirl@goddess.moe>
2016-03-11 14:53:23 -0500

    Keyboard turning for NEWTICRATERATIO

--- src/g_game.c
+++ src/g_game.c
@@ -946,7 +946,7 @@ angle_t localangle, localangle2;
 
 static fixed_t forwardmove[2] = {25<<FRACBITS>>16, 50<<FRACBITS>>16};
 static fixed_t sidemove[2] = {25<<FRACBITS>>16, 50<<FRACBITS>>16}; // faster!
-static fixed_t angleturn[3] = {640, 1280, 320}; // + slow turn
+static fixed_t angleturn[3] = {640/NEWTICRATERATIO, 1280/NEWTICRATERATIO, 320/NEWTICRATERATIO}; // + slow turn
 
 void G_BuildTiccmd(ticcmd_t *cmd, INT32 realtics)
 {
