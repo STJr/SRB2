65d9c9e1671e0a5c5080f71cef0cdda305a99382
Monster Iestyn <iestynjealous@ntlworld.com>
2016-05-27 14:49:11 +0100

    P_NetUnArchivePlayers doesn't like having "inline" either

--- src/p_saveg.c
+++ src/p_saveg.c
@@ -280,7 +280,7 @@ static void P_NetArchivePlayers(void)
 //
 // P_NetUnArchivePlayers
 //
-static inline void P_NetUnArchivePlayers(void)
+static void P_NetUnArchivePlayers(void)
 {
 	INT32 i, j;
 	UINT16 flags;
