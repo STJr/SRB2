c0f5f22b6a11d66f68ff72ba1127f640964dfacf
toasterbabe <rollerorbital@gmail.com>
2016-08-28 13:51:32 +0100

    Suggested improvement by MI on irc.

--- src/p_mobj.c
+++ src/p_mobj.c
@@ -1278,7 +1278,7 @@ fixed_t P_GetMobjGravity(mobj_t *mo)
 
 		for (rover = mo->subsector->sector->ffloors; rover; rover = rover->next)
 		{
-			if (!((rover->flags & FF_EXISTS) && (P_InsideANonSolidFFloor(mo, rover))))
+			if (!(rover->flags & FF_EXISTS) || !P_InsideANonSolidFFloor(mo, rover))
 				continue;
 
 			if ((rover->flags & (FF_SWIMMABLE|FF_GOOWATER)) == (FF_SWIMMABLE|FF_GOOWATER))
