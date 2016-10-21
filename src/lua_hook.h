fcf2fe79e00dae735cf383ce04422a6548fe4288
Prisima the Fox <prisimathefox@gmail.com>
2016-10-20 23:25:47 -0400

    Oops; PlayerQuit isn't a boolean!

--- src/lua_hook.h
+++ src/lua_hook.h
@@ -78,6 +78,6 @@ boolean LUAh_LinedefExecute(line_t *line, mobj_t *mo, sector_t *sector); // Hook
 boolean LUAh_PlayerMsg(int source, int target, int flags, char *msg); // Hook for chat messages
 boolean LUAh_HurtMsg(player_t *player, mobj_t *inflictor, mobj_t *source); // Hook for hurt messages
 #define LUAh_PlayerSpawn(player) LUAh_PlayerHook(player, hook_PlayerSpawn) // Hook for G_SpawnPlayer
-boolean LUAh_PlayerQuit(player_t *plr, int reason); // Hook for player quitting
+void LUAh_PlayerQuit(player_t *plr, int reason); // Hook for player quitting
 
 #endif
