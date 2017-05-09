29122a2f48e2bf2a762eec82c7d468ec200b0602
Monster Iestyn <iestynjealous@ntlworld.com>
2017-05-09 19:57:21 +0100

    prevent invalid nodes from crashing Net_CloseConnection, print a warning and return instead

--- src/d_net.c
+++ src/d_net.c
@@ -716,6 +716,12 @@ void Net_CloseConnection(INT32 node)
 	if (!node)
 		return;
 
+	if (node >= MAXNETNODES) // prevent invalid nodes from crashing the game
+	{
+		CONS_Alert(CONS_WARNING, M_GetText("Net_CloseConnection: invalid node %d detected!\n"), node);
+		return;
+	}
+
 	nodes[node].flags |= NF_CLOSE;
 
 	// try to Send ack back (two army problem)
