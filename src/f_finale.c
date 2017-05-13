ba4275c415cf5fe021619d0d9b1f694dd38210f2
toasterbabe <rollerorbital@gmail.com>
2017-05-13 22:38:06 +0100

    title screen hud hook

--- src/f_finale.c
+++ src/f_finale.c
@@ -34,6 +34,10 @@
 #include "p_local.h"
 #include "p_setup.h"
 
+#ifdef HAVE_BLUA
+#include "lua_hud.h"
+#endif
+
 // Stage of animation:
 // 0 = text, 1 = art screen
 static INT32 finalecount;
@@ -1524,7 +1528,11 @@ void F_TitleScreenDrawer(void)
 
 	// rei|miru: use title pics?
 	if (hidetitlepics)
+#ifdef HAVE_BLUA
+		goto luahook;
+#else
 		return;
+#endif
 
 	V_DrawScaledPatch(30, 14, 0, ttwing);
 
@@ -1562,6 +1570,11 @@ void F_TitleScreenDrawer(void)
 	}
 
 	V_DrawScaledPatch(48, 142, 0,ttbanner);
+
+#ifdef HAVE_BLUA
+luahook:
+	LUAh_TitleHUD();
+#endif
 }
 
 // (no longer) De-Demo'd Title Screen
