94c7d9bc52c19e5f7595b99166711ba18fe57330
toasterbabe <rollerorbital@gmail.com>
2018-05-03 00:00:22 +0100

    Forgot to do this before making the previous commit; completes support for no-points drone badniks.

--- src/p_mobj.c
+++ src/p_mobj.c
@@ -8347,7 +8347,7 @@ mobj_t *P_SpawnMobj(fixed_t x, fixed_t y, fixed_t z, mobjtype_t type)
 	mobj->height = info->height;
 	mobj->flags = info->flags;
 
-	mobj->health = info->spawnhealth;
+	mobj->health = (info->spawnhealth ? info->spawnhealth : 1);
 
 	mobj->reactiontime = info->reactiontime;
 
