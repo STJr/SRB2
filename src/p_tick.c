9ff491dd78f295412170832ff268d0b5aec24c99
Steel Titanium <steeltitanium1@gmail.com>
2018-05-08 18:36:47 -0400

    Add indentation

--- src/p_tick.c
+++ src/p_tick.c
@@ -608,7 +608,7 @@ void P_Ticker(boolean run)
 
 	// Keep track of how long they've been playing!
 	if (!demoplayback) // Don't incerment if a demo is playing.
-	totalplaytime++;
+		totalplaytime++;
 
 	if (!useNightsSS && G_IsSpecialStage(gamemap))
 		P_DoSpecialStageStuff();
