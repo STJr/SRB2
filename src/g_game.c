76c8d30ed2c927d68511d928d6d1b6c89e3d06b7
Monster Iestyn <iestynjealous@ntlworld.com>
2018-06-30 18:14:04 +0100

    Fix special stage map end var defaults to use the correct map numbers

--- src/g_game.c
+++ src/g_game.c
@@ -3204,7 +3204,7 @@ void G_LoadGameSettings(void)
 	// defaults
 	spstage_start = 1;
 	sstage_start = smpstage_start = 50;
-	sstage_end = smpstage_end = 57; // 7 special stages in vanilla SRB2
+	sstage_end = smpstage_end = 56; // 7 special stages in vanilla SRB2
 	sstage_end++; // plus one weirdo
 
 	// initialize free sfx slots for skin sounds
