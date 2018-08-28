73a66d22dfceb789acd589cb1177a83dd5c5deab
Monster Iestyn <iestynjealous@ntlworld.com>
2018-08-28 20:57:43 +0100

    At toaster's suggestion, have 2p control whether closed captioning is moved up if the powerup display is 1st person only

--- src/screen.c
+++ src/screen.c
@@ -451,8 +451,11 @@ void SCR_ClosedCaptions(void)
 			basey -= 8;
 		else if ((modeattacking == ATTACKING_NIGHTS)
 		|| (!(maptol & TOL_NIGHTS)
-		&& ((cv_powerupdisplay.value == 2)
-		|| (cv_powerupdisplay.value == 1 && !splitscreen && !camera.chase))))
+		&& ((cv_powerupdisplay.value == 2) // "Always"
+		|| (cv_powerupdisplay.value == 1 // "First-person only"
+			&& ((!splitscreen && !camera.chase)
+			|| (splitscreen && !camera2.chase)) // in 2p mode, move up only if 2p's powerup display is on
+			))))
 			basey -= 16;
 	}
 
