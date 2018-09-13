3fc8ed5a9f05d8a387517ae657f7e224cdddb196
mazmazz <mar.marcoz@outlook.com>
2018-09-13 10:53:03 -0400

    455: Set timing by either Back Y or Front Y, but not line length

--- src/p_spec.c
+++ src/p_spec.c
@@ -3446,7 +3446,9 @@ static void P_ProcessLineSpecial(line_t *line, mobj_t *mo, sector_t *callsec)
 					Z_Free(exc);
 
 				Add_ColormapFader(&sectors[secnum], source_exc, dest_exc, (line->flags & ML_EFFECT4), // tic-based timing
-					(line->sidenum[1] != 0xFFFF ? abs(sides[line->sidenum[1]].rowoffset >> FRACBITS) : abs(P_AproxDistance(line->dx, line->dy) >> FRACBITS)));
+					((line->flags & ML_DONTPEGBOTTOM) || !sides[line->sidenum[0]].rowoffset) && line->sidenum[1] != 0xFFFF ?
+						abs(sides[line->sidenum[1]].rowoffset >> FRACBITS)
+						: abs(sides[line->sidenum[0]].rowoffset >> FRACBITS));
 			}
 			break;
 
