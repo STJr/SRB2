4cba996a89218e27cdd761fd9207b69d78c4a3d1
mazmazz <mar.marcoz@outlook.com>
2018-09-18 12:41:49 -0400

    Define cv_midiplayer/soundfontpath/timiditypath in s_sound.c when NOMIXER=1

--- src/sdl/mixer_sound.c
+++ src/sdl/mixer_sound.c
@@ -149,6 +149,8 @@ static void MidiSoundfontPath_Onchange(void)
 	}
 }
 
+// make sure that s_sound.c does not already verify these
+// which happens when: defined(HAVE_MIXERX) && !defined(HAVE_MIXER)
 static CV_PossibleValue_t midiplayer_cons_t[] = {{MIDI_OPNMIDI, "OPNMIDI"}, {MIDI_Fluidsynth, "Fluidsynth"}, {MIDI_Timidity, "Timidity"}, {MIDI_Native, "Native"}, {0, NULL}};
 consvar_t cv_midiplayer = {"midiplayer", "OPNMIDI" /*MIDI_OPNMIDI*/, CV_CALL|CV_NOINIT|CV_SAVE, midiplayer_cons_t, Midiplayer_Onchange, 0, NULL, NULL, 0, 0, NULL};
 consvar_t cv_midisoundfontpath = {"midisoundfont", "sf2/8bit.sf2", CV_CALL|CV_NOINIT|CV_SAVE, NULL, MidiSoundfontPath_Onchange, 0, NULL, NULL, 0, 0, NULL};
