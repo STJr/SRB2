49c678d2e9a0f8aff6d0fbb6bdbb7a9b11ed80c1
mazmazz <mar.marcoz@outlook.com>
2018-09-19 19:19:18 -0400

    MP Core s_sound: Mixed D+C fix (buildbots)

--- src/s_sound.c
+++ src/s_sound.c
@@ -1557,10 +1557,11 @@ static void S_ChangeMusicToQueue(void)
 
 void S_ChangeMusicEx(const char *mmusic, UINT16 mflags, boolean looping, UINT32 position, UINT32 prefadems, UINT32 fadeinms)
 {
+	char newmusic[7];
+
 	if (S_MusicDisabled())
 		return;
 
-	char newmusic[7];
 	strncpy(newmusic, mmusic, 7);
 #if defined(HAVE_BLUA) && defined(HAVE_LUA_MUSICPLUS)
 	if(LUAh_MusicChange(music_name, newmusic, &mflags, &looping, &position, &prefadems, &fadeinms))
