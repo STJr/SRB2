6184f91dd3bd82164282c7379d457ffcea5e5a8c
Sryder <sryder13@gmail.com>
2018-10-13 23:01:11 +0100

    Add an int to I_PlaySound to tell an interface which channel number SRB2 is using. I've voided this out on other sound interfaces than SDL Mixer ones because I'm both not sure whether they need it, and not sure how to make them work with it if they do.

--- src/djgppdos/i_sound.c
+++ src/djgppdos/i_sound.c
@@ -165,9 +165,11 @@ INT32 I_StartSound ( sfxenum_t     id,
                    INT32         vol,
                    INT32         sep,
                    INT32         pitch,
-                   INT32         priority )
+                   INT32         priority
+				   INT32         channel)
 {
 	int voice;
+	(void)channel;
 
 	if (nosound)
 	return 0;
