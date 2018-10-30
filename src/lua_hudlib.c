68414585469fbb4e1c739253e9561b430dcaf0eb
Latapostrophe <hyperclassic3@gmail.com>
2018-10-30 22:51:05 +0100

    got rid of the settop as well

--- src/lua_hudlib.c
+++ src/lua_hudlib.c
@@ -613,7 +613,6 @@ static int lib_huddisable(lua_State *L)
 static int lib_hudenabled(lua_State *L)
 {
 	enum hud option = luaL_checkoption(L, 1, NULL, hud_disable_options);
-	lua_settop(L, 2);
 	if (hud_enabled[option/8] & (1<<(option%8)))
 		lua_pushboolean(L, true);
 	else
