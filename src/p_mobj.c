dbb4924348bb5eb977282ffd7aa98b49e616d21e
mazmazz <mar.marcoz@outlook.com>
2018-11-13 10:37:34 -0500

    Compile errors

--- src/p_mobj.c
+++ src/p_mobj.c
@@ -7453,7 +7453,7 @@ void P_MobjThinker(mobj_t *mobj)
 
 			// failure state: mobj is not facing tracer
 			// Reasaonable defaults: ANGLE_67h, ANGLE_292h
-			if (ang >= mobj->extravalue1 && ang <= ANGLE_MAX - mobj->extravalue1)
+			if (ang >= (UINT32)mobj->extravalue1 && ang <= ANGLE_MAX - (UINT32)mobj->extravalue1)
 			{
 				if (mobj->cvmem)
 					mobj->cvmem--;
