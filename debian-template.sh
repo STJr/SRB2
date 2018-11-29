6d058d02bd96e357372164e136d2e1e426622e18
mazmazz <mar.marcoz@outlook.com>
2018-11-29 05:55:47 -0500

    Move debian folders to debian-template

--- debian-template.sh
+++ debian-template.sh
@@ -3,10 +3,15 @@
 # Travis-CI Deployer
 # Debian package templating
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
 # Initialize package parameter defaults
 if [[ "$__DEBIAN_PARAMETERS_INITIALIZED" != "1" ]]; then
-    . ./deployer.sh;
+    . ${DIR}/travis/deployer.sh;
 fi;
 
 __PACKAGE_DATETIME="$(date '+%a, %d %b %Y %H:%M:%S %z')"
 __PACKAGE_DATETIME_DIGIT="$(date '+%Y%m%d%H%M%S')"
+
+# TODO: Script arguments
+# Build main, or assets
