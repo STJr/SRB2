c24b95aed624b3b33b1c1c74b8179d35f30f87d2
mazmazz <mar.marcoz@outlook.com>
2018-11-29 14:02:36 -0500

    Stray comment

--- travis/deployer_build.sh
+++ travis/deployer_build.sh
@@ -16,8 +16,14 @@ if [[ "$__DEPLOYER_FTP_ACTIVE" == "1" ]]; then
 	if [[ "$_DEPLOYER_FTP_PACKAGE" == "1" ]]; then
 		echo "Building a Package for FTP";
 
+		# Make an OSX package; superuser is required for library bundling
+		#
+		# HACK: OSX packaging can't write libraries to .app package unless we're superuser
+		# because the original library files don't have WRITE permission
+		# Bug may be sidestepped by using CHMOD_BUNDLE_ITEMS=TRUE
+		# But I don't know where this is set. Not `cmake -D...` because this var is ignored.
+		# https://cmake.org/Bug/view.php?id=9284
 		if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-			# Make an OSX package; superuser is required for library bundling
 			sudo make -k package;
 		else
 			if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
