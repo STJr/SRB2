2c6154bbb32190371a6faba290293af787423b33
mazmazz <mar.marcoz@outlook.com>
2018-11-29 15:11:17 -0500

    Global trigger log message [Deployer-osx]

--- travis/deployer.sh
+++ travis/deployer.sh
@@ -88,6 +88,9 @@ if [[ "$DEPLOYER_ENABLED" == "1" ]] && [[ "$TRAVIS_PULL_REQUEST" == "false" ]]; 
             fi;
         fi;
     else
+        if [[ "$DEPLOYER_TRIGGER" != "" ]]; then
+            echo "Testing for global trigger [$DEPLOYER_TRIGGER, commit message: $TRAVIS_COMMIT_MESSAGE";
+        fi;
         if [[ "$DEPLOYER_TRIGGER" != "" ]] && [[ $TRAVIS_COMMIT_MESSAGE == *"[$DEPLOYER_TRIGGER"* ]]; then
             if [[ "$DEPLOYER_BRANCHES" == "" ]] || [[ $DEPLOYER_BRANCHES == *"$TRAVIS_BRANCH"* ]]; then
                 # Assume that some job received the trigger, so mark this for early termination
