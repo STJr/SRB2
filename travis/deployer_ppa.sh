0d5d7cff488c990d5cf3ebc6909d896269cbb193
mazmazz <mar.marcoz@outlook.com>
2018-11-30 12:49:29 -0500

    Quote fix

--- travis/deployer_ppa.sh
+++ travis/deployer_ppa.sh
@@ -17,9 +17,9 @@ if [[ "$__DEPLOYER_PPA_ACTIVE" == "1" ]]; then
         cd ../..; # level above repo root
 
         # debsign -k ${DEPLOYER_PPA_KEY_FINGERPRINT} ${PACKAGEFILENAME}.dsc \
-        #     -p'gpg --passphrase-file $OLDPWD/phrase.txt --batch --no-use-agent';
+        #     -p"gpg --passphrase-file $OLDPWD/phrase.txt --batch --no-use-agent";
         debsign ${PACKAGEFILENAME}_source.changes \
-            -p'gpg --passphrase-file $OLDPWD/phrase.txt --batch';
+            -p"gpg --passphrase-file $OLDPWD/phrase.txt --batch";
 
         dput ppa:${DEPLOYER_PPA_PATH} "${PACKAGEFILENAME}_source.changes";
         cd $OLDPWD;
@@ -31,9 +31,9 @@ if [[ "$__DEPLOYER_PPA_ACTIVE" == "1" ]]; then
         cd ..; # repo root
 
         # debsign -k ${DEPLOYER_PPA_KEY_FINGERPRINT} ${PACKAGEFILENAME}.dsc \
-        #     -p'gpg --passphrase-file $OLDPWD/phrase.txt --batch --no-use-agent';
+        #     -p"gpg --passphrase-file $OLDPWD/phrase.txt --batch --no-use-agent";
         debsign ${PACKAGEFILENAME}_source.changes \
-            -p'gpg --passphrase-file $OLDPWD/phrase.txt --batch --no-use-agent';
+            -p"gpg --passphrase-file $OLDPWD/phrase.txt --batch --no-use-agent";
         dput ppa:${DEPLOYER_PPA_PATH} "${PACKAGEFILENAME}_source.changes";
         cd $OLDPWD;
     fi;
