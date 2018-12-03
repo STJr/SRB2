6ca1130551ab765abaf9566998cc5963e4a24023
mazmazz <mar.marcoz@outlook.com>
2018-12-03 13:21:16 -0500

    Hardcode Screenshot, GIF, and Viewpoint to their F keys

--- src/g_game.c
+++ src/g_game.c
@@ -1678,7 +1678,7 @@ boolean G_Responder(event_t *ev)
 {
 	// allow spy mode changes even during the demo
 	if (gamestate == GS_LEVEL && ev->type == ev_keydown
-		&& (ev->data1 == gamecontrol[gc_viewpoint][0] || ev->data1 == gamecontrol[gc_viewpoint][1]))
+		&& (ev->data1 == KEY_F12 || ev->data1 == gamecontrol[gc_viewpoint][0] || ev->data1 == gamecontrol[gc_viewpoint][1]))
 	{
 		if (splitscreen || !netgame)
 			displayplayer = consoleplayer;
