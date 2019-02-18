bfbb83df23323ff0a57e952e48721db8e417e26a
Steel Titanium <steeltitanium1@gmail.com>
2019-02-18 02:04:58 -0500

    Save when quitting the game.
    Also use default reason if not custom reason is given.

--- src/sdl12/i_system.c
+++ src/sdl12/i_system.c
@@ -2282,6 +2282,9 @@ void I_Quit(void)
 	quiting = SDL_FALSE;
 	I_ShutdownConsole();
 	M_SaveConfig(NULL); //save game config, cvars..
+#ifndef NONET
+	D_SaveBan(); // save the ban list
+#endif
 	G_SaveGameData(); // Tails 12-08-2002
 	//added:16-02-98: when recording a demo, should exit using 'q' key,
 	//        but sometimes we forget and use 'F10'.. so save here too.
