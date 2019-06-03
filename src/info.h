d4dac52766e5841a3aa01373445a2f2a70448594
Steel Titanium <steeltitanium1@gmail.com>
2019-06-03 15:23:48 -0400

    Upped freeslots limit

--- src/info.h
+++ src/info.h
@@ -210,7 +210,7 @@ void A_NapalmScatter();
 void A_SpawnFreshCopy();
 
 // ratio of states to sprites to mobj types is roughly 6 : 1 : 1
-#define NUMMOBJFREESLOTS 256
+#define NUMMOBJFREESLOTS 512
 #define NUMSPRITEFREESLOTS NUMMOBJFREESLOTS
 #define NUMSTATEFREESLOTS (NUMMOBJFREESLOTS*8)
 
