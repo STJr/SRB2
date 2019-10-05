4ac136f6269ceb90c37c494dd971aca3f294546d
James R <justsomejames2@gmail.com>
2019-10-05 13:15:36 -0700

    Custom gamedata parameter

--- src/g_game.c
+++ src/g_game.c
@@ -3361,6 +3361,11 @@ void G_LoadGameData(void)
 	if (M_CheckParm("-resetdata"))
 		return; // Don't load (essentially, reset).
 
+	if (M_CheckParm("-gamedata") && M_IsNextParm())
+	{
+		strlcpy(gamedatafilename, M_GetNextParm(), sizeof gamedatafilename);
+	}
+
 	length = FIL_ReadFile(va(pandf, srb2home, gamedatafilename), &savebuffer);
 	if (!length) // Aw, no game data. Their loss!
 		return;
