a58c7946917b3754b01d5419f0eddd6b6f54dbb8
Nev3r <apophycens@gmail.com>
2019-10-27 12:03:52 +0100

    Missing ;

--- src/m_menu.c
+++ src/m_menu.c
@@ -10523,7 +10523,7 @@ static void M_DrawJoystick(void)
 			compareval = cv_usejoystick.value;
 #else
 		compareval2 = cv_usejoystick2.value;
-		compareval = cv_usejoystick.value
+		compareval = cv_usejoystick.value;
 #endif
 
 		if ((setupcontrols_secondaryplayer && (i == compareval2))
