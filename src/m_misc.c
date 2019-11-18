efac76f8335c25242fdb76a69ab15cca153c8890
Steel Titanium <steeltitanium1@gmail.com>
2019-11-18 15:39:41 -0500

    Merge branch 'public_next' into 'master'

--- src/m_misc.c
+++ src/m_misc.c
@@ -23,6 +23,8 @@
 #include <unistd.h>
 #endif
 
+#include <errno.h>
+
 // Extended map support.
 #include <ctype.h>
 
@@ -2440,4 +2442,14 @@ void M_SetupMemcpy(void)
 #if 0
 	M_Memcpy = cpu_cpy;
 #endif
+}
+
+/** Return the appropriate message for a file error or end of file.
+*/
+const char *M_FileError(FILE *fp)
+{
+	if (ferror(fp))
+		return strerror(errno);
+	else
+		return "end-of-file";
 }
