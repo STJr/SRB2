d1d1098f7caee6274be52185e984886ba8509e89
Monster Iestyn <iestynjealous@ntlworld.com>
2019-11-19 12:31:20 -0500

    fix object type restrictions for popping monitors
    (MT_BOUNCERING to MT_GRENADERING are actually the ammo pickups, MT_THROWNBOUNCE to MT_THROWNGRENADE are what actually want here)

--- src/p_inter.c
+++ src/p_inter.c
@@ -3532,7 +3532,7 @@ boolean P_DamageMobj(mobj_t *target, mobj_t *inflictor, mobj_t *source, INT32 da
 
 		// Make sure that boxes cannot be popped by enemies, red rings, etc.
 		if (target->flags & MF_MONITOR && ((!source || !source->player || source->player->bot)
-		|| (inflictor && inflictor->type >= MT_REDRING && inflictor->type <= MT_GRENADERING)))
+		|| (inflictor && (inflictor->type == MT_REDRING || (inflictor->type >= MT_THROWNBOUNCE && inflictor->type <= MT_THROWNGRENADE)))))
 			return false;
 	}
 
