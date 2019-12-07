69c08f93bbec94a36c5a9207a16e56d9d993519f
Jaime Passos <lazymyuutsu@gmail.com>
2019-12-07 11:55:53 -0300

    Update d_main.c

--- src/d_main.c
+++ src/d_main.c
@@ -969,7 +969,7 @@ void D_SRB2Main(void)
 	// Print GPL notice for our console users (Linux)
 	CONS_Printf(
 	"\n\nSonic Robo Blast 2\n"
-	"Copyright (C) 1998-2018 by Sonic Team Junior\n\n"
+	"Copyright (C) 1998-2019 by Sonic Team Junior\n\n"
 	"This program comes with ABSOLUTELY NO WARRANTY.\n\n"
 	"This is free software, and you are welcome to redistribute it\n"
 	"and/or modify it under the terms of the GNU General Public License\n"
