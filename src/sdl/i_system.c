553ad46c74cd99b77300589ec9a592370b80441e
James R <justsomejames2@gmail.com>
2019-12-13 15:04:31 -0800

    Add this back for Windoodoo because I'm an idiot

--- src/sdl/i_system.c
+++ src/sdl/i_system.c
@@ -2482,6 +2482,10 @@ void I_ShutdownSystem(void)
 {
 	INT32 c;
 
+#ifndef NEWSIGNALHANDLER
+	I_ShutdownConsole();
+#endif
+
 	for (c = MAX_QUIT_FUNCS-1; c >= 0; c--)
 		if (quit_funcs[c])
 			(*quit_funcs[c])();
