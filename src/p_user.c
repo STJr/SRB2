25f8f2706bc51a4d65abb4729d7ce44b8b73e8e2
GoldenTails <milestailsprower101n2@gmail.com>
2019-12-24 13:55:19 -0600

    Also cap dashspeed to at most maxdash.

--- src/p_user.c
+++ src/p_user.c
@@ -4637,6 +4637,10 @@ static void P_DoSpinAbility(player_t *player, ticcmd_t *cmd)
 					}
 					if (player->dashspeed < player->mindash)
 						player->dashspeed = player->mindash;
+
+					if (player->dashspeed < player->maxdash)
+						player->dashspeed = player->maxdash;
+					
 					if (player->dashspeed < player->maxdash && player->mindash != player->maxdash)
 					{
 #define chargecalculation (6*(player->dashspeed - player->mindash))/(player->maxdash - player->mindash)
