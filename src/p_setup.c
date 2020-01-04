72f23a1075eb0e6b8d745bd8f2c24cb511aa57bb
Nev3r <apophycens@gmail.com>
2020-01-04 23:01:01 +0100

    Add missing initialization on vertex heights for binary maps.

--- src/p_setup.c
+++ src/p_setup.c
@@ -846,6 +846,8 @@ static void P_LoadVertices(UINT8 *data)
 	{
 		v->x = SHORT(mv->x)<<FRACBITS;
 		v->y = SHORT(mv->y)<<FRACBITS;
+		vt->floorzset = vt->ceilingzset = false;
+		vt->floorz = vt->ceilingz = 0;
 	}
 }
 
