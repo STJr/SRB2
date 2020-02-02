07d71778dc8093b88f5c5665f55b83fb042e4ae0
Sally Cochenour <tehrealsalt@gmail.com>
2020-02-01 22:11:21 -0500

    FIX COLOR BUG

--- src/w_wad.c
+++ src/w_wad.c
@@ -1475,7 +1475,7 @@ void W_FlushCachedPatches(void)
 		Z_FreeTag(PU_HWRPATCHINFO);
 		Z_FreeTag(PU_HWRMODELTEXTURE);
 		Z_FreeTag(PU_HWRCACHE);
-		Z_FreeTags(PU_HWRCACHE_UNLOCKED, PU_HWRPATCHINFO_UNLOCKED);
+		Z_FreeTags(PU_HWRCACHE_UNLOCKED, PU_HWRMODELTEXTURE_UNLOCKED);
 	}
 	needpatchflush = false;
 }
