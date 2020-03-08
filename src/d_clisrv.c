fe346bf8e079565b454ca8dd7164da95130f0400
Steel Titanium <steeltitanium1@gmail.com>
2020-03-08 15:02:42 -0400

    Add missing break

--- src/d_clisrv.c
+++ src/d_clisrv.c
@@ -3616,6 +3616,7 @@ static void HandlePacketFromPlayer(SINT8 node)
 			break;
 		case PT_CANRECEIVEGAMESTATE:
 			PT_CanReceiveGamestate(node);
+			break;
 #ifdef HAVE_BLUA
 		case PT_ASKLUAFILE:
 			if (server && luafiletransfers && luafiletransfers->nodestatus[node] == LFTNS_ASKED)
