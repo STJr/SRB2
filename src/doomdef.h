ea631715a6d2ca05c8b5c2f00faee243fd37f868
Louis-Antoine <lamr@free.fr>
2020-03-22 15:17:16 +0100

    Remove unused define

--- src/doomdef.h
+++ src/doomdef.h
@@ -562,10 +562,6 @@ extern const char *compdate, *comptime, *comprevision, *compbranch;
 // None of these that are disabled in the normal build are guaranteed to work perfectly
 // Compile them at your own risk!
 
-/// Backwards compatibility with SRB2CB's slope linedef types.
-///	\note	A simple shim that prints a warning.
-#define ESLOPE_TYPESHIM
-
 ///	Allows the use of devmode in multiplayer. AKA "fishcake"
 //#define NETGAME_DEVMODE
 
