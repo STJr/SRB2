da3ab1a14c04b98ebf913fae05a408c4d87953e6
GoldenTails <milestailsprower101n2@gmail.com>
2020-06-06 07:50:58 -0500

    Ok fiiineee, I'll support 2.2

--- src/dehacked.c
+++ src/dehacked.c
@@ -10810,6 +10810,12 @@ static inline int lib_getenum(lua_State *L)
 			lua_pushinteger(L, (lua_Integer)PF_FULLSTASIS);
 			return 1;
 		}
+		else if (fastcmp(p, "USEDOWN")) // Remove case when 2.3 nears release...
+		{
+			LUA_Deprecated(L, "PF_USEDOWN", "PF_SPINDOWN");
+			lua_pushinteger(L, (lua_Integer)PF_SPINDOWN);
+			return 1;
+		}
 		if (mathlib) return luaL_error(L, "playerflag '%s' could not be found.\n", word);
 		return 0;
 	}
@@ -11074,6 +11080,13 @@ static inline int lib_getenum(lua_State *L)
 				return 1;
 			}
 		return 0;
+	}
+
+	if (fastcmp(word, "BT_USE")) // Remove case when 2.3 nears release...
+	{
+		LUA_Deprecated(L, "BT_USE", "BT_SPIN");
+		lua_pushinteger(L, (lua_Integer)BT_SPIN);
+		return 1;
 	}
 
 	for (i = 0; INT_CONST[i].n; i++)
