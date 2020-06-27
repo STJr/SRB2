fef15be0afedc46dc2b970add95b3f08f1b819b7
GoldenTails <milestailsprower101n2@gmail.com>
2020-06-27 10:56:15 -0500

    Create a slightly more dynamic, optional frame delay system to show off your terrible hardware with

--- src/d_netcmd.c
+++ src/d_netcmd.c
@@ -675,6 +675,7 @@ void D_RegisterClientCommands(void)
 	// GIF variables
 	CV_RegisterVar(&cv_gif_optimize);
 	CV_RegisterVar(&cv_gif_downscale);
+	CV_RegisterVar(&cv_gif_dynamicdelay);
 	CV_RegisterVar(&cv_gif_localcolortable);
 
 #ifdef WALLSPLATS
