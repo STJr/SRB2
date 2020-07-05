KartKrewDev/RingRacers

Set up with the font system

src/hu_stuff.h
@@ -17,6 +17,7 @@
 #include "d_event.h"
 #include "w_wad.h"
 #include "r_defs.h"
+#include "font.h"
 
 //------------------------------------
 //           heads up font
@@ -41,6 +42,23 @@
 #define CRED_FONTSTART '!' // the first font character
 #define CRED_FONTEND 'Z' // the last font character
 #define CRED_FONTSIZE (CRED_FONTEND - CRED_FONTSTART + 1)
+
+#define X( name ) name ## _FONT
+/* fonts */
+enum
+{
+	X        (HU),
+	X      (TINY),
+	X      (KART),
+
+	X        (LT),
+	X      (CRED),
+
+	X   (TALLNUM),
+	X (NIGHTSNUM),
+	X   (PINGNUM),
+};
+#undef  X
 
 #define HU_CROSSHAIRS 3 // maximum of 9 - see HU_Init();
 
@@ -78,15 +96,9 @@ void HU_AddChatText(const char *text, boolean playsound);
 // set true when entering a chat message
 extern boolean chat_on;
 
-extern patch_t *hu_font[HU_FONTSIZE], *kart_font[KART_FONTSIZE], *tny_font[HU_FONTSIZE];	// SRB2kart
-extern patch_t *tallnum[10];
-extern patch_t *pingnum[10];
 extern patch_t *pinggfx[5];
-extern patch_t *nightsnum[10];
 extern patch_t *framecounter;
 extern patch_t *frameslash;
-extern patch_t *lt_font[LT_FONTSIZE];
-extern patch_t *cred_font[CRED_FONTSIZE];
 extern patch_t *emeraldpics[7];
 extern patch_t *tinyemeraldpics[7];
 extern patch_t *rflagico;
@@ -103,6 +115,9 @@ extern boolean hu_showscores;
 void HU_Init(void);
 
 void HU_LoadGraphics(void);
+
+// Load a HUDGFX patch or NULL.
+patch_t *HU_CachePatch(const char *format, ...);
 
 // reset heads up when consoleplayer respawns.
 void HU_Start(void);

STJr/SRB2

src/hu_stuff.h
@@ -17,6 +17,7 @@
 #include "d_event.h"
 #include "w_wad.h"
 #include "r_defs.h"
+#include "font.h"
 
 //------------------------------------
 //           heads up font
@@ -40,6 +41,26 @@
 #define NT_FONTSTART '!' // the first font character
 #define NT_FONTEND 'Z' // the last font character
 #define NT_FONTSIZE (NT_FONTEND - NT_FONTSTART + 1)
+
+#define X( name ) name ## _FONT
+/* fonts */
+enum
+{
+	X        (HU),
+	X      (TINY),
+	X      (KART),
+
+	X        (LT),
+	X      (CRED),
+
+	X       (NTB),
+	X       (NTO),
+
+	X   (TALLNUM),
+	X (NIGHTSNUM),
+	X    (TTLNUM),
+};
+#undef  X
 
 #define HU_CROSSHAIRS 3 // maximum of 9 - see HU_Init();
 
@@ -78,14 +99,6 @@ void HU_AddChatText(const char *text, boolean playsound);
 // set true when entering a chat message
 extern boolean chat_on;
 
-extern patch_t *hu_font[HU_FONTSIZE], *tny_font[HU_FONTSIZE];
-extern patch_t *tallnum[10];
-extern patch_t *nightsnum[10];
-extern patch_t *lt_font[LT_FONTSIZE];
-extern patch_t *cred_font[CRED_FONTSIZE];
-extern patch_t *ntb_font[NT_FONTSIZE];
-extern patch_t *nto_font[NT_FONTSIZE];
-extern patch_t *ttlnum[10];
 extern patch_t *emeraldpics[3][8];
 extern patch_t *rflagico;
 extern patch_t *bflagico;
@@ -103,6 +116,9 @@ extern boolean hu_showscores;
 void HU_Init(void);
 
 void HU_LoadGraphics(void);
+
+// Load a HUDGFX patch or NULL.
+patch_t *HU_CachePatch(const char *format, ...);
 
 // reset heads up when consoleplayer respawns.
 void HU_Start(void);