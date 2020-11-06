faf16a6ae42d83a5f750eb0fe826f82867db970c
James R <justsomejames2@gmail.com>
2020-11-05 20:05:04 -0800

    Let access spectator mobj

--- src/lua_playerlib.c
+++ src/lua_playerlib.c
@@ -112,12 +112,7 @@ static int player_get(lua_State *L)
 	// Kept for backward-compatibility
 	// Should be fixed to work like "realmo" later
 	else if (fastcmp(field,"mo"))
-	{
-		if (plr->spectator)
-			lua_pushnil(L);
-		else
-			LUA_PushUserdata(L, plr->mo, META_MOBJ);
-	}
+		LUA_PushUserdata(L, plr->mo, META_MOBJ);
 	else if (fastcmp(field,"cmd"))
 		LUA_PushUserdata(L, &plr->cmd, META_TICCMD);
 	else if (fastcmp(field,"playerstate"))
