284205baacdd2ffb923ef222a3469178f9ddd12f
Steel Titanium <steeltitanium1@gmail.com>
2020-12-06 22:20:06 -0500

    Fix SPC looping on libgme versions >= 0.6.3

--- src/sdl/mixer_sound.c
+++ src/sdl/mixer_sound.c
@@ -1298,6 +1298,9 @@ boolean I_PlaySong(boolean looping)
 	if (gme)
 	{
 		gme_equalizer_t eq = {GME_TREBLE, GME_BASS, 0,0,0,0,0,0,0,0};
+#if GME_VERSION >= 0x000603
+        gme_set_autoload_playback_limit(gme, 0);
+#endif        
 		gme_set_equalizer(gme, &eq);
 		gme_start_track(gme, 0);
 		current_track = 0;
