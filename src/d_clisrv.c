858cb98e57020be1cf822a114025d79f9d43c57c
katsy <katmint@live.com>
2021-04-30 17:26:09 -0500

    attempt to resolve teleportation resync issue

--- src/d_clisrv.c
+++ src/d_clisrv.c
@@ -4268,7 +4268,7 @@ static void HandlePacketFromPlayer(SINT8 node)
 		case PT_RECEIVEDGAMESTATE:
 			sendingsavegame[node] = false;
 			resendingsavegame[node] = false;
-			savegameresendcooldown[node] = I_GetTime() + 15 * TICRATE;
+			savegameresendcooldown[node] = I_GetTime() + TICRATE;
 			break;
 // -------------------------------------------- CLIENT RECEIVE ----------
 		case PT_SERVERTICS:
