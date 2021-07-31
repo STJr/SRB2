df99cde40ffd1163cfec164e72f7b5d3b31261cb
Monster Iestyn <iestynjealous@ntlworld.com>
2021-07-31 22:11:44 +0100

    mo not mobj!

--- src/p_mobj.c
+++ src/p_mobj.c
@@ -2550,7 +2550,7 @@ boolean P_ZMovement(mobj_t *mo)
 
 		P_CheckPosition(mo, mo->x, mo->y); // Sets mo->standingslope correctly
 
-		if (P_MobjWasRemoved(mobj)) // mobjs can be removed by P_CheckPosition -- Monster Iestyn 31/07/21
+		if (P_MobjWasRemoved(mo)) // mobjs can be removed by P_CheckPosition -- Monster Iestyn 31/07/21
 			return false;
 
 		if (((mo->eflags & MFE_VERTICALFLIP) ? tmceilingslope : tmfloorslope) && (mo->type != MT_STEAM))
