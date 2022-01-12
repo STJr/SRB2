56c0c1f415ccca94d62b67751f68bcad7f1ab4eb
James R <justsomejames2@gmail.com>
2022-01-12 04:30:03 -0800

    Do not speed up underwater/heatwave effect in OpenGL

--- src/hardware/hw_main.c
+++ src/hardware/hw_main.c
@@ -6711,7 +6711,6 @@ void HWR_DoPostProcessor(player_t *player)
 		// 10 by 10 grid. 2 coordinates (xy)
 		float v[SCREENVERTS][SCREENVERTS][2];
 		static double disStart = 0;
-		static fixed_t last_fractime = 0;
 
 		UINT8 x, y;
 		INT32 WAVELENGTH;
@@ -6743,16 +6742,7 @@ void HWR_DoPostProcessor(player_t *player)
 		}
 		HWD.pfnPostImgRedraw(v);
 		if (!(paused || P_AutoPause()))
-			disStart += 1;
-		if (renderdeltatics > FRACUNIT)
-		{
-			disStart = disStart - FIXED_TO_FLOAT(last_fractime) + 1 + FIXED_TO_FLOAT(rendertimefrac);
-		}
-		else
-		{
-			disStart = disStart - FIXED_TO_FLOAT(last_fractime) + FIXED_TO_FLOAT(rendertimefrac);
-		}
-		last_fractime = rendertimefrac;
+			disStart += FIXED_TO_FLOAT(renderdeltatics);
 
 		// Capture the screen again for screen waving on the intermission
 		if(gamestate != GS_INTERMISSION)
