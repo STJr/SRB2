91561b72776cef3fedd733a4c5dab5d5bde33062
spherallic <spherallic@gmail.com>
2022-02-21 21:52:04 +0100

    2.2.10 release candidate 1

--- src/version.h
+++ src/version.h
@@ -1,4 +1,4 @@
-#define SRB2VERSION "2.2.9"/* this must be the first line, for cmake !! */
+#define SRB2VERSION "2.2.10"/* this must be the first line, for cmake !! */
 
 // The Modification ID; must be obtained from a Master Server Admin ( https://mb.srb2.org/members/?key=ms_admin ).
 // DO NOT try to set this otherwise, or your modification will be unplayable through the Master Server.
@@ -12,4 +12,4 @@
 #define MODVERSION 50
 
 // Define this as a prerelease version suffix
-// #define BETAVERSION "RC1"
+#define BETAVERSION "RC1"
