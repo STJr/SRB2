f0279e69f296f07113a457cb52ca33d270e17495
Eidolon <furyhunter600@gmail.com>
2022-04-11 19:41:27 -0500

    Reset mobj interp state on TeleportMove

--- src/p_map.c
+++ src/p_map.c
@@ -103,6 +103,8 @@ boolean P_TeleportMove(mobj_t *thing, fixed_t x, fixed_t y, fixed_t z)
 	thing->floorrover = tmfloorrover;
 	thing->ceilingrover = tmceilingrover;
 
+	P_ResetInterpolationState(thing);
+
 	return true;
 }
 
