c3923adc3f5cd52bdc33114acf4b7837c3fa70c6
Eidolon <furyhunter600@gmail.com>
2022-04-27 17:08:40 -0500

    Make Lua TeleportMove call MoveOrigin

--- src/lua_baselib.c
+++ src/lua_baselib.c
@@ -1783,8 +1783,8 @@ static int lib_pTeleportMove(lua_State *L)
 	INLEVEL
 	if (!thing)
 		return LUA_ErrInvalid(L, "mobj_t");
-	LUA_Deprecated(L, "P_TeleportMove", "P_SetOrigin or P_MoveOrigin");
-	lua_pushboolean(L, P_SetOrigin(thing, x, y, z));
+	LUA_Deprecated(L, "P_TeleportMove", "P_SetOrigin\" or \"P_MoveOrigin");
+	lua_pushboolean(L, P_MoveOrigin(thing, x, y, z));
 	LUA_PushUserdata(L, tmthing, META_MOBJ);
 	P_SetTarget(&tmthing, ptmthing);
 	return 2;
