56261d3e1e1040b9610828d37d3d9e6f1142a209
Eidolon <furyhunter600@gmail.com>
2022-04-29 17:19:09 -0500

    Fix offset copies for SpawnMobjFromMobj
    THANKS SAL

--- src/p_mobj.c
+++ src/p_mobj.c
@@ -14013,10 +14013,10 @@ mobj_t *P_SpawnMobjFromMobj(mobj_t *mobj, fixed_t xofs, fixed_t yofs, fixed_t zo
 
 	newmobj->old_x2 = mobj->old_x2 + xofs;
 	newmobj->old_y2 = mobj->old_y2 + yofs;
-	newmobj->old_z2 = mobj->old_z2 + yofs;
+	newmobj->old_z2 = mobj->old_z2 + zofs;
 	newmobj->old_x = mobj->old_x + xofs;
 	newmobj->old_y = mobj->old_y + yofs;
-	newmobj->old_z = mobj->old_z + yofs;
+	newmobj->old_z = mobj->old_z + zofs;
 
 	// This angle hack is needed for Lua scripts that set the angle after
 	// spawning, to avoid erroneous interpolation.
@@ -14030,7 +14030,6 @@ mobj_t *P_SpawnMobjFromMobj(mobj_t *mobj, fixed_t xofs, fixed_t yofs, fixed_t zo
 		newmobj->old_angle2 = mobj->old_angle2;
 		newmobj->old_angle = mobj->old_angle;
 	}
-	newmobj->resetinterp = true;
 
 	newmobj->old_scale2 = mobj->old_scale2;
 	newmobj->old_scale = mobj->old_scale;
