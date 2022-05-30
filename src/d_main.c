8b175398f8165720cd3751e1aa98a366b182acb2
Sally Coolatta <tehrealsalt@gmail.com>
2022-05-30 18:22:46 -0400

    Use old behavior when dipping below 35 with interp

--- src/d_main.c
+++ src/d_main.c
@@ -853,7 +853,7 @@ void D_SRB2Loop(void)
 
 			renderdeltatics = FLOAT_TO_FIXED(deltatics);
 
-			if (!(paused || P_AutoPause()) && !hu_stopped)
+			if (!(paused || P_AutoPause()) && deltatics < 1.0 && !hu_stopped)
 			{
 				rendertimefrac = g_time.timefrac;
 			}
