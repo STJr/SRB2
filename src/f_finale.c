311e0911a8e88336719ed9324d909c8a909da18d
spherallic <spherallic@gmail.com>
2023-01-11 13:53:59 +0100

    Fix continue screen & console for gamepad input

--- src/f_finale.c
+++ src/f_finale.c
@@ -3828,7 +3828,7 @@ boolean F_ContinueResponder(event_t *event)
 
 	if (timetonext >= 21*TICRATE/2)
 		return false;
-	if (event->type != ev_keydown)
+	if (type != ev_keydown)
 		return false;
 
 	// remap virtual keys (mouse & gamepad buttons)
