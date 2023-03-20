84222677e065dd8c4bc8c6bb35d71d034f42ac5e
James R <justsomejames2@gmail.com>
2023-03-20 14:42:44 -0700

    Fix code formatting in P_SetTarget

--- src/p_tick.c
+++ src/p_tick.c
@@ -293,10 +293,18 @@ void P_RemoveThinker(thinker_t *thinker)
 
 mobj_t *P_SetTarget(mobj_t **mop, mobj_t *targ)
 {
-	if (*mop)              // If there was a target already, decrease its refcount
+	if (*mop) // If there was a target already, decrease its refcount
+	{
 		(*mop)->thinker.references--;
-if ((*mop = targ) != NULL) // Set new target and if non-NULL, increase its counter
+	}
+
+	if (targ != NULL) // Set new target and if non-NULL, increase its counter
+	{
 		targ->thinker.references++;
+	}
+
+	*mop = targ;
+
 	return targ;
 }
 
