0f53ec317ded098fd53eeb9b426ff5b6ead79fb3
spherallic <spherallic@gmail.com>
2023-05-26 02:57:06 +0200

    Actually register hitbox interpolation cvar

--- src/d_netcmd.c
+++ src/d_netcmd.c
@@ -874,6 +874,7 @@ void D_RegisterClientCommands(void)
 	CV_RegisterVar(&cv_fullscreen);
 	CV_RegisterVar(&cv_renderview);
 	CV_RegisterVar(&cv_renderhitbox);
+	CV_RegisterVar(&cv_renderhitboxinterpolation);
 	CV_RegisterVar(&cv_renderer);
 	CV_RegisterVar(&cv_scr_depth);
 	CV_RegisterVar(&cv_scr_width);
