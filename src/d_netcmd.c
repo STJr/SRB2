ccd60bc0335acd992e426c22d9d0c8535ad49b32
Zwip-Zwap Zapony <zwipzwapzapony@gmail.com>
2023-05-26 12:23:34 +0000

    Swap registration order for hitbox rendering cvars

--- src/d_netcmd.c
+++ src/d_netcmd.c
@@ -873,8 +873,8 @@ void D_RegisterClientCommands(void)
 	// screen.c
 	CV_RegisterVar(&cv_fullscreen);
 	CV_RegisterVar(&cv_renderview);
-	CV_RegisterVar(&cv_renderhitbox);
 	CV_RegisterVar(&cv_renderhitboxinterpolation);
+	CV_RegisterVar(&cv_renderhitbox);
 	CV_RegisterVar(&cv_renderer);
 	CV_RegisterVar(&cv_scr_depth);
 	CV_RegisterVar(&cv_scr_width);
