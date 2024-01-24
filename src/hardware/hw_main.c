98d80053dc7b8348db322c2ce500b7b20b0f3f35
Lactozilla <jp6781615@gmail.com>
2024-01-24 02:21:04 -0300

    Use the vertex coords passed in to the function instead of gr_curline

--- src/hardware/hw_main.c
+++ src/hardware/hw_main.c
@@ -1389,12 +1389,12 @@ static void HWR_RenderExtraTexture(unsigned which, v2d_t vs, v2d_t ve, float xcl
 		else
 		{
 			high = max(
-				P_GetSectorCeilingZAt(gl_frontsector, gl_curline->v1->x, gl_curline->v1->y),
-				P_GetSectorCeilingZAt(gl_frontsector, gl_curline->v2->x, gl_curline->v2->y)
+				P_GetSectorCeilingZAt(gl_frontsector, v1x, v1y),
+				P_GetSectorCeilingZAt(gl_frontsector, v2x, v2y)
 			);
 			low = min(
-				P_GetSectorFloorZAt(gl_backsector, gl_curline->v1->x, gl_curline->v1->y),
-				P_GetSectorFloorZAt(gl_backsector, gl_curline->v2->x, gl_curline->v2->y)
+				P_GetSectorFloorZAt(gl_backsector, v1x, v1y),
+				P_GetSectorFloorZAt(gl_backsector, v2x, v2y)
 			);
 		}
 
