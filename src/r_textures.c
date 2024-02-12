356e4553353e7eecf32af8146b2b5f3456fcbc59
Lactozilla <jp6781615@gmail.com>
2024-02-12 16:58:00 -0300

    Fix #1190

--- src/r_textures.c
+++ src/r_textures.c
@@ -147,7 +147,7 @@ static void R_DrawFlippedColumnInCache(column_t *column, UINT8 *cache, texpatch_
 
 		if (count > 0)
 		{
-			for (; dest < cache + position + count; --source)
+			for (; dest < cache + position + count; --source, is_opaque++)
 			{
 				*dest++ = *source;
 				*is_opaque = true;
@@ -191,7 +191,7 @@ static void R_DrawBlendColumnInCache(column_t *column, UINT8 *cache, texpatch_t 
 
 		if (count > 0)
 		{
-			for (; dest < cache + position + count; source++, dest++)
+			for (; dest < cache + position + count; source++, dest++, is_opaque++)
 			{
 				*dest = ASTBlendPaletteIndexes(*dest, *source, originPatch->style, originPatch->alpha);
 				*is_opaque = true;
@@ -235,7 +235,7 @@ static void R_DrawBlendFlippedColumnInCache(column_t *column, UINT8 *cache, texp
 
 		if (count > 0)
 		{
-			for (; dest < cache + position + count; --source, dest++)
+			for (; dest < cache + position + count; --source, dest++, is_opaque++)
 			{
 				*dest = ASTBlendPaletteIndexes(*dest, *source, originPatch->style, originPatch->alpha);
 				*is_opaque = true;
