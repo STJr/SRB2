2d17c751653b765f0df59e0d52e883baba0db3a2
Logan Aerl Arias <logana@srb2.org>
2024-02-17 19:50:52 -0500

    Update r_defs.h
    add back pic_t

--- src/r_defs.h
+++ src/r_defs.h
@@ -25,6 +25,10 @@
 
 #include "screen.h" // MAXVIDWIDTH, MAXVIDHEIGHT
 
+#ifdef HWRENDER
+#include "m_aatree.h"
+#endif
+
 #include "taglist.h"
 
 //
@@ -901,6 +905,26 @@ typedef struct
 	INT32 columnofs[8];     // only [width] used
 	// the [0] is &columnofs[width]
 } ATTRPACK softwarepatch_t;
+
+#ifdef _MSC_VER
+#pragma warning(disable :  4200)
+#endif
+
+// a pic is an unmasked block of pixels, stored in horizontal way
+typedef struct
+{
+	INT16 width;
+	UINT8 zero;       // set to 0 allow autodetection of pic_t
+	                 // mode instead of patch or raw
+	UINT8 mode;       // see pic_mode_t above
+	INT16 height;
+	INT16 reserved1; // set to 0
+	UINT8 data[0];
+} ATTRPACK pic_t;
+
+#ifdef _MSC_VER
+#pragma warning(default : 4200)
+#endif
 
 #if defined(_MSC_VER)
 #pragma pack()
