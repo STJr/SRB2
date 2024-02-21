de13b600bf3cbfef49e6ff556e2842bd2d0fd787
Arthur <spaddlewit@gmail.com>
2024-02-20 21:42:23 -0500

    R_IsPointInSector is fine, it was the clang issue screwing up

--- src/p_mobj.c
+++ src/p_mobj.c
@@ -1120,10 +1120,7 @@ fixed_t P_MobjFloorZ(sector_t *sector, sector_t *boundsec, fixed_t x, fixed_t y,
 		testy += y;
 
 		// If the highest point is in the sector, then we have it easy! Just get the Z at that point
-//		if (R_IsPointInSector(boundsec ? boundsec : sector, testx, testy))
-//			return P_GetSlopeZAt(slope, testx, testy);
-
-		if (R_PointInSubsector(testx, testy)->sector == (boundsec ? boundsec : sector)) // Faster
+		if (R_IsPointInSector(boundsec ? boundsec : sector, testx, testy))
 			return P_GetSlopeZAt(slope, testx, testy);
 
 		// If boundsec is set, we're looking for specials. In that case, iterate over every line in this sector to find the TRUE highest/lowest point
@@ -1199,10 +1196,7 @@ fixed_t P_MobjCeilingZ(sector_t *sector, sector_t *boundsec, fixed_t x, fixed_t 
 		testy += y;
 
 		// If the highest point is in the sector, then we have it easy! Just get the Z at that point
-//		if (R_IsPointInSector(boundsec ? boundsec : sector, testx, testy))
-//			return P_GetSlopeZAt(slope, testx, testy);
-
-		if (R_PointInSubsector(testx, testy)->sector == (boundsec ? boundsec : sector)) // Faster
+		if (R_IsPointInSector(boundsec ? boundsec : sector, testx, testy))
 			return P_GetSlopeZAt(slope, testx, testy);
 
 		// If boundsec is set, we're looking for specials. In that case, iterate over every line in this sector to find the TRUE highest/lowest point
