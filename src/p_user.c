d43d5fb9e14ebf417077cd9e31be861a3939d7c5
SSNTails <spaddlewit@gmail.com>
2024-02-23 15:23:39 -0500

    Don't change drawangle while riding on a fan

--- src/p_user.c
+++ src/p_user.c
@@ -12134,6 +12134,8 @@ void P_PlayerThink(player_t *player)
 				case CR_DUSTDEVIL:
 					player->drawangle += ANG20;
 					break;
+				case CR_FAN: // Don't impact drawangle in any special way when on a fan
+					break;
 				/* -- in case we wanted to have the camera freely movable during zoom tubes
 				case CR_ZOOMTUBE:*/
 				case CR_ROPEHANG:
