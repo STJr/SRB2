6c628db56a265115438cab865832455d12e42d5c
Logan Aerl Arias <logana@srb2.org>
2024-04-03 00:54:35 -0400

    1st try around sorting the lumps by name

--- src/filesrch.c
+++ src/filesrch.c
@@ -699,6 +699,13 @@ static void initdirpath(char *dirpath, size_t *dirpathindex, int depthleft)
 		dirpathindex[depthleft]--;
 }
 
+//sortdir by name? 
+static int lumpnamecompare( const void *A, const void *B )
+{
+	return strcmp( (((lumpinfo_t *)A)->fullname), (((lumpinfo_t *)B)->fullname));
+
+}
+
 lumpinfo_t *getdirectoryfiles(const char *path, UINT16 *nlmp, UINT16 *nfolders)
 {
 	DIR **dirhandle;
@@ -888,6 +895,9 @@ lumpinfo_t *getdirectoryfiles(const char *path, UINT16 *nlmp, UINT16 *nfolders)
 
 	free(dirpathindex);
 	free(dirhandle);
+
+	//sort files and directories
+	qsort ( lumpinfo, numlumps, sizeof( lumpinfo_t ), lumpnamecompare );
 
 	(*nlmp) = numlumps;
 	return lumpinfo;
