4001c933c42e1ae0df5dcee0361ba384b573ac17
BarrelsOFun <161785144+BarrelsOFun@users.noreply.github.com>
2024-07-13 20:43:38 -0700

    Adds 'hidden' SOC value for Unlockables to absolutely hide unlockable from checklist

--- src/m_menu.c
+++ src/m_menu.c
@@ -7284,8 +7284,8 @@ static void M_HandleChecklist(INT32 choice)
 				{
 					if (!unlockables[j].name[0])
 						continue;
-					// if (unlockables[j].nochecklist)
-					//	continue;
+					 if (unlockables[j].hidden)
+						continue;
 					if (!unlockables[j].conditionset)
 						continue;
 					if (unlockables[j].conditionset > MAXCONDITIONSETS)
@@ -7309,8 +7309,8 @@ static void M_HandleChecklist(INT32 choice)
 				{
 					if (!unlockables[j].name[0])
 						continue;
-					// if (unlockables[j].nochecklist)
-					//	continue;
+					 if (unlockables[j].hidden)
+						continue;
 					if (!unlockables[j].conditionset)
 						continue;
 					if (unlockables[j].conditionset > MAXCONDITIONSETS)
@@ -7363,7 +7363,7 @@ static void M_DrawChecklist(void)
 
 	while (i < MAXUNLOCKABLES)
 	{
-		if (unlockables[i].name[0] == 0 //|| unlockables[i].nochecklist
+		if (unlockables[i].name[0] == 0 || unlockables[i].hidden
 		|| !unlockables[i].conditionset || unlockables[i].conditionset > MAXCONDITIONSETS
 		|| (!data->unlocked[i] && unlockables[i].showconditionset && !M_Achieved(unlockables[i].showconditionset, data)))
 		{
@@ -7375,11 +7375,11 @@ static void M_DrawChecklist(void)
 
 		for (j = i+1; j < MAXUNLOCKABLES; j++)
 		{
-			if (!(unlockables[j].name[0] == 0 //|| unlockables[j].nochecklist
+			if (!(unlockables[j].name[0] == 0 || unlockables[j].hidden
 			|| !unlockables[j].conditionset || unlockables[j].conditionset > MAXCONDITIONSETS))
 				break;
 		}
-		if ((j != MAXUNLOCKABLES) && (unlockables[i].conditionset == unlockables[j].conditionset))
+		if ((j != MAXUNLOCKABLES) && (unlockables[i].conditionset == unlockables[j].conditionset) && !unlockables[j].hidden)
 			addy(8)
 		else
 		{
