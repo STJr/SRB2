07642b9ae1e3047f3e8241ce51ea0f62bfeddd6d
luigi-budd <90123055+luigi-budd@users.noreply.github.com>
2025-04-04 23:28:21 -0500

    Fix countdown beep playing multiple times at once

--- src/st_stuff.c
+++ src/st_stuff.c
@@ -698,7 +698,7 @@ static void ST_drawRaceNum(INT32 time)
 	if (bounce < 3)
 	{
 		height -= (2 - bounce);
-		if (!(P_AutoPause() || paused) && !bounce)
+		if (!(P_AutoPause() || paused) && !bounce &&  renderisnewtic)
 				S_StartSound(0, ((racenum == racego) ? sfx_s3kad : sfx_s3ka7));
 	}
 	V_DrawScaledPatch(((BASEVIDWIDTH - racenum->width)/2), height, V_PERPLAYER, racenum);
