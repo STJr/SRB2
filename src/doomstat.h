409fa6a2a537dba1ba2e3d119a0c536396f36151
Skydusk <lightacecz@gmail.com>
2025-08-09 20:04:25 +0000

    Reduce spaces

--- src/doomstat.h
+++ src/doomstat.h
@@ -250,10 +250,10 @@ extern textprompt_t *textprompts[MAX_PROMPTS];
  
 // EXIT_ prefix is already used. These flags are for exitmapflags variable
 typedef enum {
-	EXITMAP_SKIPSTATS    = 1, // Skips Score Tally
+	EXITMAP_SKIPSTATS = 1, // Skips Score Tally
 	EXITMAP_SKIPCUTSCENE = 1<<1, // Skips Post-Level Cutscene
-	EXITMAP_SKIPSPECIAL  = 1<<2, // Skips Special Stages
-	EXITMAP_SKIPRECORDS  = 1<<3, // Skips Records Check
+	EXITMAP_SKIPSPECIAL = 1<<2, // Skips Special Stages
+	EXITMAP_SKIPRECORDS = 1<<3, // Skips Records Check
 	EXITMAP_NOTIMEATTACK = 1<<4, // Skips Awarding Time Attack Emblems
 } mapexitflags_t;
 
