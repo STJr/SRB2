7e99031b496e380a5338b7f0466045cff4fc3f1c
MIDIMan <miditheman2.0@gmail.com>
2026-07-16 18:41:04 -0400

    Comment out DEVELOP define

--- src/doomdef.h
+++ src/doomdef.h
@@ -140,7 +140,7 @@ extern char logfilename[1024];
 /* A mod name to further distinguish versions. */
 #define SRB2APPLICATION "SRB2"
 
-#define DEVELOP // Disable this for release builds to remove excessive cheat commands and enable MD5 checking and stuff, all in one go. :3
+//#define DEVELOP // Disable this for release builds to remove excessive cheat commands and enable MD5 checking and stuff, all in one go. :3
 #ifdef DEVELOP
 #define VERSIONSTRING "Development EXE"
 #define VERSIONSTRING_RC "Development EXE" "\0"
